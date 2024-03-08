1e893cb65fbda030928b5e55bff3fe7e2a24daa5
mooneryy <yelisyeyevaa@gmail.com>
2024-03-08 15:15:17 +0200

    +

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ const swiper = new Swiper(".home", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
-    });
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+  return;
+}
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
